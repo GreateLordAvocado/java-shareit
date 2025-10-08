4ec622aef34e1c198853c31d629923a416f386cb
GreateLordAvocado <sinner.halo@yandex.ru>
2025-10-08 13:38:44 +0700

    Исправления по чекстайлу

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -6,9 +6,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+
     ItemDto create(Long ownerId, ItemDto dto);
+
     ItemDto update(Long ownerId, Long itemId, ItemDto patch);
+
     ItemDto getById(Long itemId);
+
     List<ItemDto> getByOwner(Long ownerId);
+
     List<ItemDto> search(String text);
 }
--- src/main/java/ru/practicum/shareit/user/storage/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserRepository.java
@@ -6,10 +6,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
+
     User save(User user);
+
     User update(User user);
+
     Optional<User> findById(Long id);
+
     List<User> findAll();
+
     void deleteById(Long id);
+
     boolean existsByEmail(String email, Long ignoreUserId);
 }
